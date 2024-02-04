3e37bcf639c37c36041c04e931e96ff6426d8dc6
Goran Stefanovski <tevidma@gmail.com>
2024-02-04 20:23:37 +0100

    header implementation

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import Header from '@/components/header'
 import './globals.css'
 import { Inter } from 'next/font/google'
 
@@ -22,6 +23,7 @@ export default function RootLayout({
         sm:w-[68.75rem]'></div>
         <div className='bg-[#dbd7fb] absolute -z-10 top-[-1rem] left-[-35rem] h-[31.25rem] w-[50rem] rounded-full blur-[10rem]
         sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem]'></div>
+        <Header />
         {children}
         </body>
     </html>
