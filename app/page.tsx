c0e9c7cec4c4724d63570fa0356111a3cf236dec
Goran Stefanovski <tevidma@gmail.com>
2024-08-16 14:03:59 +0200

    add google analytics

--- app/page.tsx
+++ app/page.tsx
@@ -5,10 +5,12 @@ import Intro from "@/components/intro";
 import Projects from "@/components/projects";
 import SectionDivider from "@/components/section-devider";
 import Skills from "@/components/skills";
+import GoogleAnalytics from "@/lib/googleAnalytics";
 
 export default function Home() {
   return (
     <main className="flex flex-col items-center px-4">
+      <GoogleAnalytics />
       <Intro />   
       <SectionDivider />
       <About />   
