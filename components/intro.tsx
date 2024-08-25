915791a5dd2fcccdfc5cc8cf256a872d748816b6
Goran Stefanovski <tevidma@gmail.com>
2024-08-25 23:35:58 +0200

    seo changes

--- components/intro.tsx
+++ components/intro.tsx
@@ -96,6 +96,7 @@ export default function Intro() {
 
         <a
           className="bg-white p-4 text-gray-700 hover:text-gray-950 flex items-center gap-2 rounded-full focus:scale-[1.15] hover:scale-[1.15] active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60"
+          aria-label="Twitter"
           href="https://www.linkedin.com/in/goranstefanovski/"
           target="_blank"
         >
@@ -104,6 +105,7 @@ export default function Intro() {
 
         <a
           className="bg-white p-4 text-gray-700 flex items-center gap-2 text-[1.35rem] rounded-full focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60"
+          aria-label="GitHub"
           href="https://github.com/GoranStefanovski"
           target="_blank"
         >
